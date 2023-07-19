f0c80456348442db7f763c9e1789f5ab6192970a
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-07-19 19:13:19 +0500

    fixed error

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const openBtn = document.querySelector(".nav_top--button")
 const opnBtn2 = document.querySelector(".open2")
 const submit = document.querySelector(".submit")
 const close2 = document.querySelector(".close_btn1")
+const close_win = document.querySelector(".close_window")
+const input1 = document.querySelector(".input1")
+const input2 = document.querySelector(".input2")
 
 
 openBtn.addEventListener("click", () => {
@@ -40,8 +43,26 @@ close_btn1.addEventListener("click", () => {
 })
 
 submit.addEventListener("click", () => {
-    modal.classList.add("hide")
-    modal_popup.classList.remove("hide")
+    if (input1.value == "" || input2.value == "") {
+        alert("Please fill the inputs")
+    } else {
+        setTimeout(() => 
+            modal.classList.add("hide"),
+            modal_popup.classList.remove("hide")
+        )
+    }
 })
 
-opnBtn2.addEventListener
+opnBtn2.addEventListener("click", () => {
+    header.classList.add("hide")
+    modal.classList.remove("hide")
+    blurs.classList.remove("hide")
+})
+
+close_win.addEventListener("click", () => {
+    header.classList.remove("hide")
+    modal_popup.classList.add("hide")
+    modal.classList.add("hide")
+    blurs.classList.add("hide")
+    document.body.style.overflow = "scroll"
+})
