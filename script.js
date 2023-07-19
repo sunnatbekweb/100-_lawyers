caae1350272448720fb992cff9800bcfc57f14ad
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-07-19 20:06:31 +0500

    fixed all inputs

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const modal = document.querySelector(".drop_menu")
+const modal_popup = document.querySelector(".modal_popup")
 const blurs = document.querySelector(".blur")
 const header = document.querySelector("header")
+
+const close_win = document.querySelector(".close_window")
 const close_btn1 = document.querySelector(".close_btn2")
-const modal_popup = document.querySelector(".modal_popup")
 const openBtn = document.querySelector(".nav_top--button")
-const opnBtn2 = document.querySelector(".open2")
+const openBtn2 = document.querySelector(".open2")
 const submit = document.querySelector(".submit")
 const close2 = document.querySelector(".close_btn1")
-const close_win = document.querySelector(".close_window")
+
+
 const input1 = document.querySelector(".input1")
 const input2 = document.querySelector(".input2")
 
@@ -36,10 +39,11 @@ close_btn1.addEventListener("click", () => {
     document.body.style.overflow = "scroll"
 })
 
-close_btn1.addEventListener("click", () => {
+close2.addEventListener("click", () => {
     header.classList.remove("hide")
     modal.classList.add("hide")
     blurs.classList.add("hide")
+    modal_popup.classList.add("hide")
     document.body.style.overflow = "scroll"
 })
 
@@ -47,19 +51,21 @@ submit.addEventListener("click", () => {
     if (input1.value == "" || input2.value == "") {
         alert("Please fill the inputs")
     } else {
-        setTimeout(() => 
+        setTimeout(() =>
             modal.classList.add("hide"),
             modal_popup.classList.remove("hide")
+
         )
         input1.value = ""
         input2.value = ""
     }
 })
 
-opnBtn2.addEventListener("click", () => {
+openBtn2.addEventListener("click", () => {
     header.classList.add("hide")
     modal.classList.remove("hide")
     blurs.classList.remove("hide")
+    document.body.style.overflow = "hidden"
 })
 
 close_win.addEventListener("click", () => {
@@ -68,4 +74,49 @@ close_win.addEventListener("click", () => {
     modal.classList.add("hide")
     blurs.classList.add("hide")
     document.body.style.overflow = "scroll"
+})
+
+const choose_number = document.querySelector(".choose_tel")
+const question = document.querySelector(".question_tel")
+const protal_tel = document.querySelector(".portal_tel")
+const protal_name = document.querySelector(".portal_name")
+const choose_btn = document.querySelector(".choose_btn")
+const q_btn = document.querySelector(".question_btn")
+const p_btn = document.querySelector(".portal_btn")
+
+choose_btn.addEventListener("click", () => {
+    if (choose_number.value == "") {
+        alert("Please fill input")
+    } else {
+        header.classList.add("hide")
+        modal_popup.classList.remove("hide")
+        blurs.classList.remove("hide")
+        choose_number.value = ""
+        document.body.style.overflow = "hidden"
+    }
+})
+
+q_btn.addEventListener("click", () => {
+    if (question.value == "") {
+        alert("Please fill input")
+    } else {
+        header.classList.add("hide")
+        modal_popup.classList.remove("hide")
+        blurs.classList.remove("hide")
+        question.value = ""
+        document.body.style.overflow = "hidden"
+    }
+})
+
+p_btn.addEventListener("click", () => {
+    if (protal_tel.value == "" || protal_name.value == "") {
+        alert("Please fill input")
+    } else {
+        header.classList.add("hide")
+        modal_popup.classList.remove("hide")
+        blurs.classList.remove("hide")
+        protal_tel.value = ""
+        protal_name.value = ""
+        document.body.style.overflow = "hidden"
+    }
 })
